55bf4393348e52be78e75b550e31ffbf07fe9d64
Anirudh Jwala <anirudhsai2@gmail.com>
2020-11-15 21:02:10 +0530

    Updated audio library

--- src/data.js
+++ src/data.js
@@ -1,64 +1,71 @@
 import { v4 as uuidv4 } from "uuid";
 
+import deathBed from "./audio/Death_Bed.mp3";
+import dynamite from "./audio/Dynamite.mp3";
+import mood from "./audio/Mood.mp3";
+import stuckWithU from "./audio/Stuck_With_U.mp3";
+import cradles from "./audio/Cradles.mp3";
+import skechers from "./audio/Skechers.mp3";
+
 function chillHop() {
   return [
     {
-      name: "Beaver Creek",
+      name: "death bed (coffee for your head)",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#205950", "#2ab3bf"],
+        "https://shdbdecdnems03.cdnsrv.jio.com/c.saavncdn.com/180/death-bed-English-2020-20200228133600-500x500.jpg",
+      artist: "Powfu, beabadoobee",
+      audio: deathBed,
+      color: ["#ADA5CE", "#213045"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Dynamite",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/5/5f/BTS_-_Dynamite_%28official_cover%29.png",
+      artist: "BTS",
+      audio: dynamite,
+      color: ["#FBE2E6", "#DD5B8C"],
       id: uuidv4(),
       active: true,
     },
     {
-      name: "Daylight",
+      name: "Mood",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
+        "https://upload.wikimedia.org/wikipedia/en/1/1d/24kGoldn_-_Mood.png",
+      artist: "24kgoldn",
+      audio: mood,
+      color: ["#D888D3", "#070922"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Keep Going",
+      name: "Stuck with U",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-      color: ["#CD607D", "#c94043"],
+        "https://upload.wikimedia.org/wikipedia/en/d/dc/Justin_Bieber_and_Ariana_Grande_-_Stuck_with_You.png",
+      artist: "Ariana Grande, Justine Bieber",
+      audio: stuckWithU,
+      color: ["#F6F6EE", "#888984"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Nightfall",
+      name: "Cradles",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
+        "https://shdbdecdnems09.cdnsrv.jio.com/c.saavncdn.com/585/Cradles-English-2019-20191028124508-500x500.jpg",
+      artist: "Sub Urban",
+      audio: cradles,
+      color: ["#EDDD86", "#4A5760"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Reflection",
+      name: "Skechers",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
+        "https://shdbdecdnems04.cdnsrv.jio.com/c.saavncdn.com/296/Skechers-English-2020-20200403180832-500x500.jpg",
+      artist: "DripReport",
+      audio: skechers,
+      color: ["#DEC254", "#E18BB5"],
       id: uuidv4(),
       active: false,
     },
